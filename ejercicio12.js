80d48f88c3837e129c6403e1431f3c4adb60f1e7
Juanita Zuluaga Acosta <59172512+cantsbee@users.noreply.github.com>
2025-04-25 16:40:27 +0200

    Create ejercicio12.js

---
+++ ejercicio12.js
@@ -0,0 +1,22 @@
+const mutants = [
+  { name: 'Wolverine', power: 'regeneration' },
+  { name: 'Magneto', power: 'magnetism' },
+  { name: 'Professor X', power: 'telepathy' },
+  { name: 'Jean Grey', power: 'telekinesis' },
+  { name: 'Rogue', power: 'power absorption' },
+  { name: 'Storm', power: 'weather manipulation' },
+  { name: 'Mystique', power: 'shape-shifting' },
+  { name: 'Beast', power: 'superhuman strength' },
+  { name: 'Colossus', power: 'steel skin' },
+  { name: 'Nightcrawler', power: 'teleportation' }
+];
+
+function findMutantByPower(mutants, power) {
+  const foundMutants = mutants.filter(mutant => mutant.power === power);
+
+  if (foundMutants.length > 0) {
+    return `Se encontraron ${foundMutants.length} mutante(s) con el poder ${power}: ${foundMutants.map(mutant => mutant.name).join(', ')}`;
+  } else {
+    return `No se encontraron mutantes con el poder ${power}.`;
+  }
+}
