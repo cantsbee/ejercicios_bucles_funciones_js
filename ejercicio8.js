35c96b21d82c5d6a25c167ee9a33606c2aa0e5e9
Juanita Zuluaga Acosta <59172512+cantsbee@users.noreply.github.com>
2025-04-25 16:47:23 +0200

    Create ejercicio8.js

---
+++ ejercicio8.js
@@ -0,0 +1,14 @@
+const artists = [
+  { name: 'Kurt Cobain', influences: ['The Beatles', 'Pixies', 'Lead Belly'] },
+  { name: 'David Bowie', influences: ['Little Richard', 'Chuck Berry', 'The Velvet Underground'] },
+  { name: 'Eddie Vedder', influences: ['The Who', 'Neil Young', 'Jim Morrison'] },
+  { name: 'Freddie Mercury', influences: ['Liza Minnelli', 'Jimi Hendrix', 'Aretha Franklin'] },
+  { name: 'John Lennon', influences: ['Elvis Presley', 'Chuck Berry', 'Buddy Holly'] }
+];
+
+// Bucle anidado para recorrer artistas e influencias
+for (const artist of artists) {
+  for (const influence of artist.influences) {
+    console.log(`${artist.name} se inspiró en ${influence}`);
+  }
+}
